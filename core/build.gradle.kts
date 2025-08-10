3475dd4a7b9b3e4dff3ed9fb2b609a6443768de7
JiaMing <bbs_geek@163.com>
2025-08-10 17:24:05 +0800

    Update build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,17 +1,12 @@
 import net.kyori.blossom.BlossomExtension
 
 plugins {
-    // Allow blossom to mark sources root of templates
-    idea
+    id("net.kyori.blossom")
+    id("net.kyori.indra.git")
     id("geyser.publish-conventions")
-    id("io.freefair.lombok")
 }
 
 dependencies {
-    constraints {
-        implementation(libs.raknet) // Ensure protocol does not override the RakNet version
-    }
-
     api(projects.common)
     api(projects.api)
 
@@ -25,50 +20,54 @@ dependencies {
     // Network libraries
     implementation(libs.websocket)
 
-    api(libs.bundles.protocol)
+    api(libs.protocol) {
+        exclude("com.nukkitx.network", "raknet")
+    }
 
-    api(libs.minecraftauth)
+    api("com.netease.mc.pe:authlib-test:1.2")
+    api("org.msgpack:msgpack:0.6.12")
+
+    api(libs.mcauthlib)
     api(libs.mcprotocollib) {
         exclude("io.netty", "netty-all")
-        exclude("net.raphimc", "MinecraftAuth")
+        exclude("com.github.GeyserMC", "packetlib")
+        exclude("com.github.GeyserMC", "mcauthlib")
+    }
+
+    api(libs.packetlib) {
+        exclude("io.netty", "netty-all")
     }
 
     implementation(libs.raknet) {
-        exclude("io.netty", "*")
+        exclude("io.netty", "*");
     }
 
+    implementation(libs.netty.resolver.dns)
+    implementation(libs.netty.resolver.dns.native.macos) { artifact { classifier = "osx-x86_64" } }
+    implementation(libs.netty.codec.haproxy)
 
     // Network dependencies we are updating ourselves
     api(libs.netty.handler)
-    implementation(libs.netty.codec.haproxy)
 
-    api(libs.netty.transport.native.epoll) { artifact { classifier = "linux-x86_64" } }
+    implementation(libs.netty.transport.native.epoll) { artifact { classifier = "linux-x86_64" } }
     implementation(libs.netty.transport.native.epoll) { artifact { classifier = "linux-aarch_64" } }
-    // kqueue is macos only
     implementation(libs.netty.transport.native.kqueue) { artifact { classifier = "osx-x86_64" } }
-    api(libs.netty.transport.native.io.uring) { artifact { classifier = "linux-x86_64" } }
-    implementation(libs.netty.transport.native.io.uring) { artifact { classifier = "linux-aarch_64" } }
 
     // Adventure text serialization
     api(libs.bundles.adventure)
-    api("com.netease.mc.pe:authlib-test:1.2")
-    // command library
-    api(libs.cloud.core)
-
-    api(libs.erosion.common) {
-        isTransitive = false
-    }
 
     // Test
     testImplementation(libs.junit)
-    testImplementation(libs.mockito)
 
     // Annotation Processors
     compileOnly(projects.ap)
 
     annotationProcessor(projects.ap)
+}
 
-    api(libs.events)
+configurations.api {
+    // This is still experimental - additionally, it could only really benefit standalone
+    exclude(group = "io.netty.incubator", module = "netty-incubator-transport-native-io_uring")
 }
 
 tasks.processResources {
@@ -79,7 +78,7 @@ tasks.processResources {
         expand(
             "branch" to info.branch,
             "buildNumber" to info.buildNumber,
-            "projectVersion" to info.version,
+            "projectVersion" to project.version,
             "commit" to info.commit,
             "commitAbbrev" to info.commitAbbrev,
             "commitMessage" to info.commitMessage,
@@ -88,26 +87,20 @@ tasks.processResources {
     }
 }
 
-sourceSets {
-    main {
-        blossom {
-            val info = GitInfo()
-            javaSources {
-                property("version", info.version)
-                property("gitVersion", info.gitVersion)
-                property("buildNumber", info.buildNumber.toString())
-                property("branch", info.branch)
-                property("commit", info.commit)
-                property("repository", info.repository)
-                property("devVersion", info.isDev.toString())
-            }
-        }
-    }
+configure<BlossomExtension> {
+    val mainFile = "src/main/java/org/geysermc/geyser/GeyserImpl.java"
+    val info = GitInfo()
+
+    replaceToken("\${version}", "${project.version} (${info.gitVersion})", mainFile)
+    replaceToken("\${gitVersion}", info.gitVersion, mainFile)
+    replaceToken("\${buildNumber}", info.buildNumber, mainFile)
+    replaceToken("\${branch}", info.branch, mainFile)
+    replaceToken("\${commit}", info.commit, mainFile)
+    replaceToken("\${repository}", info.repository, mainFile)
 }
 
-fun isDevBuild(branch: String, repository: String): Boolean {
-    return branch != "master" || repository.equals("https://github.com/GeyserMC/Geyser", ignoreCase = true).not()
-}
+fun Project.buildNumber(): Int =
+    System.getenv("BUILD_NUMBER")?.let { Integer.parseInt(it) } ?: -1
 
 inner class GitInfo {
     val branch: String
@@ -121,40 +114,21 @@ inner class GitInfo {
     val commitMessage: String
     val repository: String
 
-    val isDev: Boolean
-
     init {
-        branch = indraGit.branchName() ?: "DEV"
+        // On Jenkins, a detached head is checked out, so indra cannot determine the branch.
+        // Fortunately, this environment variable is available.
+        branch = indraGit.branchName() ?: System.getenv("BRANCH_NAME") ?: "DEV"
 
         val commit = indraGit.commit()
         this.commit = commit?.name ?: "0".repeat(40)
         commitAbbrev = commit?.name?.substring(0, 7) ?: "0".repeat(7)
 
         gitVersion = "git-${branch}-${commitAbbrev}"
+        version = "${project.version} ($gitVersion)"
+        buildNumber = buildNumber()
 
         val git = indraGit.git()
         commitMessage = git?.commit()?.message ?: ""
         repository = git?.repository?.config?.getString("remote", "origin", "url") ?: ""
-
-        buildNumber = buildNumber()
-        isDev = isDevBuild(branch, repository)
-        val projectVersion = if (isDev) project.version else projectVersion(project)
-        version = "$projectVersion ($gitVersion)"
     }
-}
-
-// Manual task to download the bedrock data files from the CloudburstMC/Data repository
-// Invoke with ./gradlew :core:downloadBedrockData --suffix=1_20_70
-// Set suffix to the current Bedrock version
-tasks.register<DownloadFilesTask>("downloadBedrockData") {
-    urls = listOf(
-        "https://raw.githubusercontent.com/CloudburstMC/Data/master/entity_identifiers.dat",
-        "https://raw.githubusercontent.com/CloudburstMC/Data/master/block_palette.nbt",
-        "https://raw.githubusercontent.com/CloudburstMC/Data/master/creative_items.json",
-        "https://raw.githubusercontent.com/CloudburstMC/Data/master/runtime_item_states.json",
-        "https://raw.githubusercontent.com/CloudburstMC/Data/master/stripped_biome_definitions.json"
-    )
-    suffixedFiles = listOf("block_palette.nbt", "creative_items.json", "runtime_item_states.json")
-
-    destinationDir = "$projectDir/src/main/resources/bedrock"
 }
